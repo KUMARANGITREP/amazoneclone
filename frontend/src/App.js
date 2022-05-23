e965b966e487643cad4576f593ae21cb32a46a14
KUMARAN <kumaran16102002@gmail.com>
2022-05-24 00:13:53 +0530

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazonclone</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
